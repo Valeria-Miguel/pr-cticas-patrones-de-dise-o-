2fb22bef8cc74b4e5e527b1fc6c87b68dd1f3477
Valeria-Miguel <2022371092@uteq.edu.mx>
2024-12-06 23:25:36 -0600

    Update index.js

--- Patrones diseno/Patrones de comportamiento/State/index.js
+++ Patrones diseno/Patrones de comportamiento/State/index.js
@@ -5,10 +5,10 @@ const EstadoSinAgua = require('./src/EstadoSinAgua');*/
 //verifica si cada clase de estado puede manejar una solicitud 
 try {
   let maquina = new (require('./src/MaquinaCafe'))();
-  maquina.solicitud();  //debería preparar café
+  maquina.solicitud();  //deberia preparar café
   maquina.solicitud();  //café debería estar listo y luego sin agua
-  maquina.solicitud();  //debería pedir llenar el tanque de agua
-  maquina.solicitud();  //debería preparar café de nuevo
+  maquina.solicitud();  //deberia pedir llenar el tanque de agua
+  maquina.solicitud();  //deberia preparar café de nuevo
   console.log("Todas las solicitudes se manejaron correctamente");
 } catch (e) {
   console.error("Error encontrado durante la prueba:", e.message);

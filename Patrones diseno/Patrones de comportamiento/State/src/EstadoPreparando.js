bc1bab19e27d481197c8f2f02bdcda44df116da0
Valeria-Miguel <2022371092@uteq.edu.mx>
2024-12-06 23:24:24 -0600

    Update EstadoPreparando.js

--- Patrones diseno/Patrones de comportamiento/State/src/EstadoPreparando.js
+++ Patrones diseno/Patrones de comportamiento/State/src/EstadoPreparando.js
@@ -1,12 +1,12 @@
-//este estado representa cuando la maquina esta preparando el café..
+//este estado representa cuando la maquina esta preparando el café
 const Estado = require('./Estado');
 const EstadoSinAgua = require('./EstadoSinAgua');
 
 class EstadoPreparando extends Estado {
-  //el comportamiento de este estado es preparar el café.
+  //el comportamiento de este estado es preparar el café
   manejarSolicitud(contexto) {
     console.log("café listo. disfruta!");
-    //despues de preparar el café, cambiamos al estado "sin agua".
+    //despues de preparar el café, cambiamos al estado "sin agua"
     contexto.cambiarEstado(new EstadoSinAgua());
   }
 }

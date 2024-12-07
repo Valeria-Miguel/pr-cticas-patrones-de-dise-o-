3557f5114f3f9d02f4a18a001d778a18a9656f00
Valeria-Miguel <2022371092@uteq.edu.mx>
2024-12-06 23:22:53 -0600

    Update Sujeto.js

--- Patrones diseno/Patrones de comportamiento/Observer/src/Sujeto.js
+++ Patrones diseno/Patrones de comportamiento/Observer/src/Sujeto.js
@@ -3,17 +3,17 @@ class Sujeto {
       this.observadores = [];
     }
   
-    // método para agregar un observador
+    //metodo para agregar un observador
     agregarObservador(observador) {
       this.observadores.push(observador);
     }
   
-    // método para eliminar un observador
+    //metodo para eliminar un observador
     eliminarObservador(observador) {
       this.observadores = this.observadores.filter(obs => obs !== observador);
     }
   
-    // método para notificar a todos los observadores
+    //metodo para notificar a todos los observadores
     notificarObservadores() {
       this.observadores.forEach(observador => observador.actualizar());
     }

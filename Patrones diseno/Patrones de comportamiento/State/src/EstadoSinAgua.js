d654aa95a584e7a3265c0d2f6c1f75c9c18f2ba0
Valeria-Miguel <2022371092@uteq.edu.mx>
2024-12-06 23:24:50 -0600

    Update EstadoSinAgua.js

--- Patrones diseno/Patrones de comportamiento/State/src/EstadoSinAgua.js
+++ Patrones diseno/Patrones de comportamiento/State/src/EstadoSinAgua.js
@@ -2,7 +2,7 @@ const Estado = require('./Estado');
 const EstadoListo = require('./EstadoListo');
 
 class EstadoSinAgua extends Estado {
-    //este es el método que se ejecuta cuando se hace una solicitud
+    //este es el metodo que se ejecuta cuando se hace una solicitud
   manejarSolicitud(contexto) {
     console.log("sin agua. por favor, llena el tanque.");
       //dspues de que la solicitud se maneja, cambiamos el estado de la máquina de café

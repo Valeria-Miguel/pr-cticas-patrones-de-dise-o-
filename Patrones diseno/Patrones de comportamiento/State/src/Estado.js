06b36d3bcca95cfc3d50694d559ba32e8c1bf5a4
Valeria-Miguel <2022371092@uteq.edu.mx>
2024-12-06 23:23:50 -0600

    Update Estado.js

--- Patrones diseno/Patrones de comportamiento/State/src/Estado.js
+++ Patrones diseno/Patrones de comportamiento/State/src/Estado.js
@@ -1,7 +1,7 @@
 //esta clase no se usa directamente.
 
 class Estado {
-    // Este metodo maneja las solicitudes pero debe ser implementado por una subclase.
+    // Este metodo maneja las solicitudes pero debe ser implementado por una subclase
     manejarSolicitud(contexto) {
       throw new Error("Este metodo debe ser implementado por una subclase.");
     }
